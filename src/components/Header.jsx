10b566aa1c142e29e8118a23e70090b5d53d9764
irfan <irfan>
2024-03-10 13:55:46 +0500

    nav bar added here

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,15 +4,7 @@ import Searchbar from "./Searchbar";
 import { MdOutlineDarkMode } from "react-icons/md";
 import { MdOutlineLightMode } from "react-icons/md";
 import { useState, useEffect } from "react";
-const musicCategories = [
-  "All",
-  "Trending Songs",
-  "New Songs",
-  "Old Songs",
-  "Moods & Genres",
-  "Top Playlist",
-  "Top Artist",
-];
+
 function Header() {
   const [thememode, setThemomde] = useState(() => {
     const storedTheme = localStorage.getItem("themeMode");
@@ -26,9 +18,9 @@ function Header() {
     html.classList.add(thememode ? "light" : "dark");
   }, [thememode]);
   return (
-    <nav className="flex flex-col ">
+    <nav className="flex ">
       {/* the menu is here */}
-      <div className="bg-[#f5f5f5] h-[70px] flex w-full text-white items-center px-10 gap-4 dark:bg-[#222428]  ">
+      <div className="bg-[#f5f5f5] h-[70px]  w-full text-white items-center px-10 gap-3 dark:bg-[#222428] lg:flex hidden  ">
         <div>
           <IoMenu
             size="30px"
@@ -36,7 +28,7 @@ function Header() {
           />
         </div>
         {/* logo is here */}
-        <div className="h-[40px] cursor-pointer">
+        <div className="h-[30px] cursor-pointer">
           <img
             src="https://upload.wikimedia.org/wikipedia/commons/9/91/Gaana_%28music_streaming_service%29_logo.png"
             alt="brandlog"
@@ -44,15 +36,15 @@ function Header() {
           />
         </div>
         {/* search section is here */}
-        <div className="w-[50%] mx-[20px]  ">
+        <div className="w-[50%]  ">
           <Searchbar />
         </div>
         {/* offer section is here */}
-        <div className="bg-gradient-to-r from-red-600 to-orange-500 rounded-full text-[11px] px-2 py-2 cursor-pointer">
+        <div className="bg-gradient-to-r from-red-600 to-orange-500 rounded-full text-[11px] px-2 py-2 cursor-pointer text-nowrap">
           Welcom Offer: 1 Month Trial @ 1
         </div>
         {/* get gaana plus */}
-        <div className="border-red-500 border rounded-full text-[14px] text-red-500 px-1 py-1 cursor-pointer dark:bg-white dark:text-black dark:border-transparent">
+        <div className="border-red-500 border rounded-full text-[14px] text-red-500 px-1 py-1 cursor-pointer dark:bg-white dark:text-black dark:border-transparent text-nowrap">
           Get gaana plus
         </div>
         {/* theme mode is here */}
@@ -67,15 +59,40 @@ function Header() {
           )}
         </div>
       </div>
-      <div className="flex gap-10 px-10 py-4 dark:bg-[#1B1B1B] dark:text-white justify-center text-[16px] text-gray-700  ">
-        {musicCategories.map((music, index) => (
-          <div key={index} className="relative group ">
-            <p>
-              {music}{" "}
-              <div className="h-[2px] bg-red-600 w-0 group-hover:w-full duration-300  "></div>
-            </p>
+      {/* for small screens */}
+      <div className="bg-[#f5f5f5] h-[130px]  w-full  border-black justify-center gap-6 dark:bg-[#222428] lg:hidden flex  flex-col py-2 items-center ">
+        <div className="flex justify-between w-full items-center px-[10px]">
+          <div>
+            <IoMenu
+              size="30px"
+              className="text-black cursor-pointer dark:text-white"
+            />
           </div>
-        ))}
+          {/* logo is here */}
+          <div className="h-[30px] cursor-pointer">
+            <img
+              src="https://upload.wikimedia.org/wikipedia/commons/9/91/Gaana_%28music_streaming_service%29_logo.png"
+              alt="brandlog"
+              className="h-full"
+            />
+          </div>
+          {/* search section is here */}
+
+          {/* theme mode is here */}
+          <div
+            onClick={() => setThemomde((prev) => !prev)}
+            className="cursor-pointer"
+          >
+            {thememode ? (
+              <MdOutlineDarkMode size="25" className="text-gray-500" />
+            ) : (
+              <MdOutlineLightMode size="25" className="text-gray-300" />
+            )}
+          </div>
+        </div>
+        <div className="w-[100%]  ">
+          <Searchbar />
+        </div>
       </div>
     </nav>
   );
--- src/components/Herosection.jsx
+++ src/components/Herosection.jsx
@@ -1,19 +1,47 @@
 import React from "react";
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+
 const Listofsong = [
   "https://a10.gaanacdn.com/gn_img/showcase/NOXWVRgWkq/OXWVRGgWkq/size_m_1523469605.webp",
   "https://a10.gaanacdn.com/gn_img/showcase/81l3Mye3rM/1l3Mype3rM/size_m_1641538400.webp",
-  "https://a10.gaanacdn.com/gn_img/albums/10q3Z1K52r/q3ZjOpR035/size_l_1709621312.jpg",
-  "https://a10.gaanacdn.com/gn_pl_img/playlists/P7m3GNKqxo/7m3GQwOybq/size_l_1707732163.webp",
-  "https://a10.gaanacdn.com/gn_pl_img/playlists/9En3peWXDV/n3po26rkKX/size_l_1707485217.webp",
+  "https://a10.gaanacdn.com/gn_img/showcase/4Z9bqo3yQn/Z9bqgxe3yQ/size_m_1709710163.webp",
+  "https://a10.gaanacdn.com/gn_img/showcase/9En3pqeWXD/En3p8p03XD/size_m_1709737995.webp",
+  "https://a10.gaanacdn.com/gn_img/showcase/10q3Z1K52r/0q3ZmaNK52/size_l_1707225729.webp",
+  "https://a10.gaanacdn.com/gn_img/showcase/NOXWVRgWkq/OXWVmEL3kq/size_m_1709715114.webp",
 ];
+import "../index.css";
 function Herosection() {
+  const settings = {
+    infinite: true,
+    slidesToShow: 3,
+    speed: 500,
+    autoplay: true,
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+        },
+      },
+    ],
+  };
+
   return (
-    <div>
-      <div className="h-[350px] w-[350px]">
+    <div className="w-[95%] z mx-auto">
+      <Slider {...settings}>
         {Listofsong.map((song, index) => (
-          <img src={song} alt="songImage" key={index} />
+          <div key={index} className=" px-[2px] md:px-2 ">
+            <img
+              src={song}
+              alt="songImage"
+              className="w-full h-full object-contain"
+            />
+          </div>
         ))}
-      </div>
+      </Slider>
     </div>
   );
 }
